276d6f7527e6c2ec418724b5edd1a8673135c3c5
Kamran Aghayev <kamran.aghayev@todor3d.tech>
2025-05-12 18:58:32 +0400

    add eslint boundaries & prettier

---
+++ src/app/app.tsx
@@ -0,0 +1,10 @@
+import BoardPage from '@/features/board/index';
+
+export const App = () => {
+  return (
+    <div>
+      <BoardPage />
+      <BoardCard />
+    </div>
+  );
+};
---
+++ src/features/boards-list/board-card.tsx
@@ -0,0 +1,5 @@
+const BoardCard = () => {
+  return <div>board card</div>;
+};
+
+export default BoardCard;
